3d1e65b53073b4a6d028d202b3cf0c89d3168bb2
VishvendraShukla <shukla.vishvendra14@gmail.com>
2024-12-24 19:47:28 +0530

    Initial commit

---
+++ src/main/java/com/vishvendra/procart/ProcartApplication.java
@@ -0,0 +1,13 @@
+package com.vishvendra.procart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vishvendra/procart/ProcartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishvendra.procart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
